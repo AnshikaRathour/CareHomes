433b9d1364cbc7929b03bcf0dbfdf4cdd5e45bb6
Balbir Singh Bhatia <balbirs2204@gmail.com>
2024-04-30 19:42:43 +0530

    CareHomes Final Look

---
+++ src/components/Home/InfoPage/InfoPage.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import './InfoPage.css';
+import { FaClock, FaHeadset,FaHouseUser  } from "react-icons/fa";
+import { Link } from 'react-router-dom';
+
+const InfoPage = () => {
+    return (
+        <section className="why-us mt-5 mt-md-0">
+            <div className="container">
+
+                <div className="row">
+                    <div className="col-lg-4 d-flex align-items-stretch">
+                        <div className="content">
+                            <h3>Why Choose Us?</h3>
+                            <p>
+                            Care Homes: Your one-stop solution for managing medical records, scheduling doctor appointments online, and ensuring immediate emergency assistance, all while keeping your loved ones informed and connected.
+                            </p>
+                            <div className="text-center">
+                                <Link href="/" className="more-btn">Learn More <i className="bx bx-chevron-right"></i></Link>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-lg-8 d-flex align-items-stretch">
+                        <div className="icon-boxes d-flex flex-column justify-content-center">
+                            <div className="row">
+                                <div className="col-xl-4 d-flex align-items-stretch">
+                                    <div className="icon-box mt-4 mt-xl-0">
+                                        <FaHouseUser className="icon"/>
+                                        <h4>Appointment</h4>
+                                        <small className='text-secondary'>24 Hours Service</small>
+                                        <p>Book your next doctor's appointment effortlessly with Care Homes. Our user-friendly platform allows you to schedule appointments with ease, ensuring you receive timely care tailored to your needs.</p>
+                                    </div>
+                                </div>
+                                <div className="col-xl-4 d-flex align-items-stretch">
+                                    <div className="icon-box mt-4 mt-xl-0">
+                                        <FaHeadset className="icon"/>
+                                        <h4>Emegency Cases</h4>
+                                        <h6 className='text-secondary'>+91-7878764753</h6>
+                                        <p>In emergencies, trust Care Homes to swiftly connect you with medical help. Our intuitive emergency button ensures immediate assistance while seamlessly sharing your medical history with nearby hospitals for prompt care.</p>
+                                    </div>
+                                </div>
+                                <div className="col-xl-4 d-flex align-items-stretch">
+                                    <div className="icon-box mt-4 mt-xl-0">
+                                        <FaClock className="icon"/>
+                                        <h4>Working Hours</h4>
+                                        <small className='text-secondary'>Timing schedule</small>
+                                        <ul className='list-group list-group-flush'>
+                                        <li className="list-group-item d-flex justify-content-between text-nowrap" ><p>Sun - Wed : </p> <p>8:00 - 17: 00</p></li>
+                                        <li className="list-group-item d-flex justify-content-between text-nowrap" ><p>Thus - Fri : </p> <p>9:00 - 17: 00</p></li>
+                                        <li className="list-group-item d-flex justify-content-between text-nowrap" ><p>Sat - Sun : </p> <p>10:00 - 17: 00</p></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </section>
+    )
+}
+
+export default InfoPage
---
+++ src/components/Shared/TopHeader/TopHeader.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './index.css';
+import { FaFacebookSquare, FaInstagramSquare, FaLinkedin, FaGithubSquare, FaPhoneAlt, FaEnvelope  } from "react-icons/fa";
+
+const TopHeader = () => {
+    return (
+        <div id="topbar" className="d-flex align-items-center fixed-top">
+            <div className="container d-flex justify-content-between">
+                <div className="contact-info d-flex align-items-center">
+                    <FaEnvelope className='contact-icon'/> <a href="mailto:arathour_be22@thapar.edu">arathour_be22@thapar.edu</a>
+                    <FaPhoneAlt className='contact-icon'/> <a href="tel:+91-7696580263">+91-7696580263</a> 
+                </div>
+                <div className="d-none d-lg-flex social-links align-items-center">
+                    <a href="https://www.linkedin.com/in/anshika-rathour-3a3815259?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app" target='_blank' rel="noreferrer" className="linkedin"><FaLinkedin /></a>
+                    <a href="https://github.com/Ujjalzaman" target='_blank' rel="noreferrer" className=""><FaGithubSquare /></a>
+                </div>
+            </div>
+        </div>
+    );
+};
+export default TopHeader;
